a0aa967e13eeddabd8caae6b91dc65f5e19b70e9
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-07 13:44:00 +0200

    Added username to "User" model

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from . import db
 
 class User(UserMixin,  db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(200), nullable=False)
 
