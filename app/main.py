e86e0124d1ff9d7c9d74dd6f2cc6ed7795aa9e74
Andrew Subotin <subotinandrey5@gmail.com>
2023-07-10 10:25:17 +0200

    Update question

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Blueprint, render_template, redirect, url_for, request, flash
 from flask_login import login_required, current_user
 from .models import Question, Tag
@@ -13,17 +14,18 @@ def split_tags_string(tags: str):
     # 'python 3.x, javascript, ruby on rails ' will be
     # turned in ['python-3.x', 'javascript', 'ruby-on-rails']
     # and returned
-    if tags:
-        if tags[-1] == ',':
-            tags = tags[:-1]
-        tags_list = tags.split(',')
-        print(tags_list)
-        for index, tag in enumerate(tags_list):
-            tags_list[index] = tag.strip()
 
-        for index, tag in enumerate(tags_list):
-            if ' ' in tag:
-                tags_list[index] = '-'.join(tag.split(' '))
+    if tags[-1] == ',':
+        tags = tags[:-1]
+    if tags[0] == ',':
+        tags = tags[1:]
+    tags_list = tags.split(',')
+    for index, tag in enumerate(tags_list):
+        tags_list[index] = tag.strip()
+
+    for index, tag in enumerate(tags_list):
+        if ' ' in tag:
+            tags_list[index] = '-'.join(tag.split(' '))
 
     return tags_list
 
@@ -49,6 +51,11 @@ def post_question():
         if title and len(title) > 300:
             flash('Title is too long.')
             errors = True
+
+        if title and len(title) < 10:
+            flash('Title is too short')
+            errors = True
+
         if errors:
             return render_template('main/post_question.html',
                                    title=title, details=details, tags=tags)
@@ -65,11 +72,10 @@ def post_question():
                     db.select(Tag).filter_by(name=tag)).scalar_one_or_none()
                 if existing_tag:
                     question.tags.append(existing_tag)
-                    continue
                 else:
-                    tag = Tag(name=tag)
-                    question.tags.append(tag)
-                    db.session.add(tag)
+                    new_tag = Tag(name=tag)
+                    question.tags.append(new_tag)
+                    db.session.add(new_tag)
 
         db.session.commit()
 
@@ -77,3 +83,89 @@ def post_question():
         return redirect(url_for('main.index'))
 
     return render_template('main/post_question.html')
+
+
+@bp.route('/questions/<int:id>/update/', methods=['GET', 'POST'])
+@login_required
+def update_question(id):
+
+    if request.method == 'GET':
+        # Tried to add some optimization on queries using
+        # joinedload instead of default lazy
+        question = db.session.query(Question).\
+            options(db.joinedload(Question.tags), db.joinedload(Question.user)).\
+            filter_by(id=id).first()
+
+        if not question:
+            return render_template('nonexistent.html')
+
+        if question.user != current_user:
+            return render_template('not_allowed.html')
+
+        if question.tags:
+            tags_list = list(question.tags)
+            for index, tag in enumerate(tags_list):
+                tags_list[index] = tag.name
+            tags = ','.join(tags_list)
+
+        return render_template('main/update_question.html',
+                               question=question,
+                               tags=tags)
+
+    if request.method == 'POST':
+        question = db.session.query(Question).\
+            options(db.joinedload(Question.tags), db.joinedload(Question.user)).\
+            filter_by(id=id).first()
+
+        if not question:
+            return render_template('nonexistent.html')
+
+        if question.user != current_user:
+            return render_template('not_allowed.html')
+
+        title = request.form['title']
+        details = request.form['details']
+        tags = request.form['tags']
+        errors = False
+
+        if not title:
+            flash('Title is required')
+            errors = True
+
+        if title and len(title) > 300:
+            flash('Title is too long.')
+            errors = True
+
+        if title and len(title) < 10:
+            flash('Title is too short.')
+            errors = True
+
+        if errors:
+            return render_template('main/update_question.html',
+                                   question=question, tags=tags)
+
+        question.title = title
+        question.details = details
+        question.updated = datetime.utcnow()
+
+        if tags:
+            tags = split_tags_string(tags)
+            tag_objects = []
+            for tag in tags:
+                existing_tag = db.session.execute(
+                    db.select(Tag).filter_by(name=tag)).scalar_one_or_none()
+                if existing_tag:
+                    tag_objects.append(existing_tag)
+                else:
+                    new_tag = Tag(name=tag)
+                    tag_objects.append(new_tag)
+                    db.session.add(new_tag)
+
+            question.tags.clear()
+
+            for tag_object in tag_objects:
+                question.tags.append(tag_object)
+
+        db.session.commit()
+        flash('You successfully updated your question.', 'success')
+        return redirect(url_for('main.index'))
